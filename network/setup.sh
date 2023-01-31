82697e19652839f87371ccd5e3d6a78bc8047fe1
Nylah D. Klasson <16917393+TxJson@users.noreply.github.com>
2023-01-31 19:57:58 +0000

    Add and update dependencies

--- network/setup.sh
+++ network/setup.sh
@@ -1,1 +1,28 @@
-pip install tensorflow --user
+install_dependencies()
+{
+    pip3 install -r dependencies.txt --user --no-warn-script-location
+
+    # Install YOLOv5
+    git clone https://github.com/ultralytics/yolov5  # Clone
+    cd yolov5
+    pip3 install -r requirements.txt --user --no-warn-script-location # Install
+    cd ..
+    rm -fr yolov5
+}
+
+
+version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
+min="3.7.0"
+
+parsedVersion=$(echo "${version//./}")
+minParsed=$(echo "${min//./}")
+
+if [[ "$parsedVersion" -gt minParsed ]]
+then 
+    echo "Python is $version"
+    install_dependencies
+    echo "Installation Complete"
+else
+    echo "Invalid Python version"
+    echo "Expected minimum $min"
+fi
