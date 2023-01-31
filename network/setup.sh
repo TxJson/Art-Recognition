b1d61319b7005e3df0aad7e1d1414a51a4ebaa48
Nylah D. Klasson <16917393+TxJson@users.noreply.github.com>
2023-01-31 22:07:44 +0000

    Update network/setup

--- network/setup.sh
+++ network/setup.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 install_dependencies()
 {
+    echo "Installing Dependencies"
+
     pip3 install -r dependencies.txt --user --no-warn-script-location
 
     # Install YOLOv5
@@ -8,21 +12,75 @@ install_dependencies()
     pip3 install -r requirements.txt --user --no-warn-script-location # Install
     cd ..
     rm -fr yolov5
+
+    echo "Installation Complete"
 }
 
+download_datasets()
+{
+    echo "Downloading Datasets"
+
+    [ -d "/datasets" ] && rm -fr dataset
+    mkdir datasets
+    cd datasets
+
+    # Download Roboflow Dataset
+    # https://universe.roboflow.com/raya-al/french-paintings-raya/dataset/2#
+    mkdir roboflow
+    cd roboflow
+    curl -L "https://universe.roboflow.com/ds/kSrrrhVV5h?key=twBHoIOrE4" > roboflow.zip; unzip roboflow.zip; rm roboflow.zip
+
+    cd ..
+    echo "Download Complete"
+}
+
+setup()
+{
+    if [[ "$parsedVersion" -gt minParsed ]]
+    then 
+        install_dependencies
+        download_datasets        
+    else
+        echo "Invalid Python version"
+        echo "Expected minimum $min"
+    fi
+}
+
+clean()
+{
+    # Remove datasets
+    echo "Removing Datasets"
+    rm -fr datasets
+}
 
 version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
-min="3.7.0"
+min="3.7.0" # Minimum python version
 
 parsedVersion=$(echo "${version//./}")
 minParsed=$(echo "${min//./}")
 
-if [[ "$parsedVersion" -gt minParsed ]]
-then 
-    echo "Python is $version"
-    install_dependencies
-    echo "Installation Complete"
+cleanParam="c"
+setupParam="s"
+datasetsParam="d"
+
+if [ "$#" -eq  "0" ]
+then
+    echo "No arguments supplied"
 else
-    echo "Invalid Python version"
-    echo "Expected minimum $min"
+    arg="$1"
+    # idiomatic parameter and option handling in sh
+    while test $# -gt 0
+    do
+        case $arg in
+            -$cleanParam) clean
+                ;;
+            -$setupParam) setup
+                ;;
+            -$datasetsParam) download_datasets
+                ;;
+            -*) echo "bad option $1"
+                ;;
+        esac
+        shift
+    done
 fi
