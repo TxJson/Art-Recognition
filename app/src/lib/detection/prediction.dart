f623b0e14f46278dfd34efecd9e041ad4b926ffd
Nylah D. Klasson <16917393+TxJson@users.noreply.github.com>
2023-03-02 22:08:17 +0000

    wip

---
+++ app/src/lib/detection/prediction.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Prediction {
+  int id;
+  String label;
+  double probability;
+  // Rect? position;
+
+  Prediction(this.id, this.label, this.probability);
+
+  int get getId => id;
+  String get getLabel => label;
+  double get getProbability => probability;
+  // Rect get getPosition => position!;
+
+  @override
+  String toString() {
+    return 'Prediction(id: $id, label: $label, probability: $probability)';
+  }
+}
--- app/src/lib/shared/utilities.dart
+++ app/src/lib/shared/utilities.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart' as imglib;
 
 class Utilities {
   // Adapted from:
@@ -19,5 +20,41 @@ class Utilities {
     }
 
     return col;
+  }
+
+  static String removeIfExists(String str, String match) {
+    if (str.contains(match)) {
+      return str.replaceAll(match, '');
+    }
+
+    return str;
+  }
+
+  // Standard image convertion, borrowed from: https://gist.github.com/Alby-o/fe87e35bc21d534c8220aed7df028e03
+  // CameraImage YUV420_888 -> PNG -> Image (compresion:0, filter: none)
+  // Black
+  static imglib.Image convertToYUV420(CameraImage image) {
+    var img = imglib.Image(image.width, image.height); // Create Image buffer
+
+    Plane plane = image.planes[0];
+    const int shift = (0xFF << 24);
+
+    // Fill image buffer with plane[0] from YUV420_888
+    for (int x = 0; x < image.width; x++) {
+      for (int planeOffset = 0;
+          planeOffset < image.height * image.width;
+          planeOffset += image.width) {
+        final pixelColor = plane.bytes[planeOffset + x];
+        // color: 0x FF  FF  FF  FF
+        //           A   B   G   R
+        // Calculate pixel color
+        var newVal =
+            shift | (pixelColor << 16) | (pixelColor << 8) | pixelColor;
+
+        img.data[planeOffset + x] = newVal;
+      }
+    }
+
+    return img;
   }
 }
